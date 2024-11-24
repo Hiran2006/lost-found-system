6c88f778fb77b0b9abf9574edcac89ea44481b7e
Hiran S <hirans2006@gmail.com>
2024-11-24 13:59:17 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 
 app.use(express.static("../frontend/public"));
+app.set('view engine','ejs');
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"hiran",
+    database:"lostfound"
+})
 
 app.get('/signin',(req,res)=>{
-    console.log(req.query);
+    db.query("select * from users where email=? and passwords=?",[req.query.email,req.query.password],(err,result)=>{
+        if(result ==""){
+            res.sendStatus(404);
+        }
+        else{
+            res.render('login',{name:result.name,email:result.email});
+        }
+    });
 });
 
 app.listen(8080);
